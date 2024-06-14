746828f724c35d5286f316baaec3c084b951170d
Allan <allan.sduarte@gmail.com>
2024-06-14 13:52:49 -0300

    Fix ci tests

--- config/dev.exs
+++ config/dev.exs
@@ -4,7 +4,7 @@ import Config
 config :jaya_challenge, JayaChallenge.Repo,
   username: "postgres",
   password: "postgres",
-  hostname: "db",
+  hostname: "localhost",
   database: "jaya_challenge_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
